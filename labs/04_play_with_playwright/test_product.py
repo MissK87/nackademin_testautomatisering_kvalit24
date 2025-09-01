8aa225ef98ea84b4b7c028265dedc266910fcbb4
purity kahara <puritykahara8@gmail.com>
2025-09-01 14:39:12 +0200

    lab 04

--- labs/04_play_with_playwright/test_product.py
+++ labs/04_play_with_playwright/test_product.py
@@ -6,3 +6,7 @@ def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
     expect(page).to_have_title(re.compile("Playwright"))
     expect(page.get_by_text("Get started")).to_be_visible()
+
+
+def test_has_product(page: Page):
+    page.goto("http://localhost:5173/")    
